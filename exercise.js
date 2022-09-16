9334edf7067b2b0ed3aa1b79d65bf42d54ba72d2
Arya Karimi <devaryakrm@gmal.com>
2022-09-17 00:02:20 +0300

    codes added

---
+++ exercise.js
@@ -0,0 +1,8 @@
+let primitive = 13;
+if (typeof primitive === "string") {
+  console.log("Primitive is String");
+} else if (typeof primitive === "number") {
+  console.log("Primitive is Number");
+} else {
+  console.log("Primitive is Boolean");
+}
